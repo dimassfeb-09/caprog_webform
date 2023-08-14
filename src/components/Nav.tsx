56a29668fb3907448bf9a773b3d73d641c56eb47
Dimas Febriyanto <dimassfeb@gmail.com>
2023-08-14 20:19:31 +0700

    updated header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,12 @@
 const Nav = () => {
-    return (
-        <>
-            <div
-                className="absolute h-16 w-full bg-gradient-to-r from-[#fb68fe] via-[#7ffffe] to-[#c0aaff] blur-3xl"></div>
-            <nav
-                className="sticky z-50 top-0 mb-3 flex items-center justify-center h-16 w-full backdrop-blur-md font-bold border-b">
-                <div className="text-sm">
-                    Pendaftaran Asisten dan Programmer Lab Psikologi UG
-                </div>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <div className="absolute h-16 w-full bg-gradient-to-r from-[#fb68fe] via-[#7ffffe] to-[#c0aaff] blur-3xl"></div>
+      <nav className="sticky z-50 top-0 mb-3 flex items-center justify-center h-16 w-full backdrop-blur-md font-bold border-b">
+        <div className="text-sm">Pendaftaran Asisten dan Programmer Lab</div>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
